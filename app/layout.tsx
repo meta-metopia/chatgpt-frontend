a76eb119574561e0bc66c32f9ffef47ea468cab5
sirily11 <32106111+sirily11@users.noreply.github.com>
2024-08-31 23:05:54 +0900

    fix: build error (#6)

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,23 +46,19 @@ export default function RootLayout({ children }: RootLayoutProps) {
           GeistMono.variable
         )}
       >
-        <Theme>
-          <Toaster position="top-center" />
-          <Providers
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <div className="flex flex-col min-h-screen">
-              <Header />
-              <main className="flex flex-col flex-1 bg-muted/50">
-                {children}
-              </main>
-            </div>
-            <TailwindIndicator />
-          </Providers>
-        </Theme>
+        <Toaster position="top-center" />
+        <Providers
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex flex-col flex-1 bg-muted/50">{children}</main>
+          </div>
+          <TailwindIndicator />
+        </Providers>
       </body>
     </html>
   )
